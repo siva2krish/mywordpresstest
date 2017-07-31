23188f1d18027e4c94c48515dc3fd2ef6018aa6e
siva2krish <30602781+siva2krish@users.noreply.github.com>
2017-07-31 14:19:50 -0400

    Update word-press installation-1.sh

--- word-press installation-1.sh
+++ word-press installation-1.sh
@@ -2,13 +2,13 @@
 
 ###WordPress Deployment Automation script ##
 
-
+##Need to give DB Password Manually  ##
 
 sudo apt-get -y  update
 sudo apt-get -y  install apache2
 sudo apache2ctl configtest
 apache2ctl configtest
-sudo systemctl restart apache2
+#sudo systemctl restart apache2
 sudo ufw app list
 sudo ufw app info "Apache Full"
 sudo apt-get -y  install curl
@@ -18,28 +18,29 @@ dbpass=snp001
 export DEBIAN_FRONTEND=noninteractive
 echo mysql-server mysql-server/root_password password $dbpass | debconf-set-selections
 echo mysql-server mysql-server/root_password_again password $dbpass | debconf-set-selections
-sudo apt-get -y install mysql-server*
-sudo apt-get -y  install php libapache2-mod-php php-mcrypt php-mysql
-sudo systemctl restart apache2
-sudo systemctl status apache2
+sudo apt-get -y install mysql-server
+sudo apt-get -y update;
+sudo apt-get -y install php5-mysql php5-mysql
+#sudo apt-get -y  install php libapache2-mod-php php-mcrypt php-mysql
+#sudo systemctl restart apache2
+#sudo systemctl status apache2
 #apt-cache search php- | less
 #apt-cache show package_name
-apt-cache show php-cli
-sudo apt-get -y  install php-cli
+apt-cache show php5-cli
+sudo apt-get -y  install php5-cli
 sudo apt-get -y  install  php5-mysqlnd-ms
-sudo apt-get -y  install php-mysql
-#sudo apt-get -y  install package1 package2 ...
+sudo apt-get -y  install php5-mysql
 dbusername="root"
 dbpass="snp001"
 echo "create DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" |  mysql -u $dbusername -p$dbpass
-#echo "GRANT ALL ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY 'snp0001';" | mysql -u $dbusername -p$dbpass
+echo "GRANT ALL ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY 'snp0001';" | mysql -u $dbusername -p$dbpass
 echo "FLUSH PRIVILEGES;" |  mysql -u $dbusername -p$dbpass
-echo "EXIT;" | mysql -u $dbusername -p$dbpass
+echo "exit;" | mysql -u $dbusername -p$dbpass
 sudo apt-get -y  update
-sudo apt-get -y  install php-curl* php-gd* php-mbstring* php-mcrypt* php-xml* php-xmlrpc*
-sudo systemctl restart apache2
+sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
+sudo /etc/init.d/apache2 restart
 sudo apache2ctl configtest
-sudo systemctl restart apache2
+sudo /etc/init.d/apache2 restart
 cd /tmp
 curl -O https://wordpress.org/latest.tar.gz
 tar xzvf latest.tar.gz
@@ -54,8 +55,6 @@ sudo chmod g+w /var/www/html/wp-content
 sudo chmod -R g+w /var/www/html/wp-content/themes
 sudo chmod -R g+w /var/www/html/wp-content/plugins
 curl -s https://api.wordpress.org/secret-key/1.1/salt/
-cd /var/www/html
-mv wp-config.php /tmp/
 wget http://220.226.22.115/configfiles/wp-config.php1
 mv  wp-config.php1 /home/
 cd /home/
